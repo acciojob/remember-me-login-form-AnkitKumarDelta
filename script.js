657119adedf3cf2ddd4d15c12ae9b0dfc2508a19
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-10-17 09:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ let check = document.querySelector('#checkbox');
 let submitBtn = document.querySelector('#submit');
 let loginBtn = document.querySelector('#existing');
 
+// Hide existing user button initially
 loginBtn.style.display = "none";
 
-if (localStorage.getItem("userdata")) {
+// Show if credentials exist
+if (localStorage.getItem("username") && localStorage.getItem("password")) {
   loginBtn.style.display = "inline";
 }
 
@@ -15,21 +17,19 @@ f.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (check.checked) {
-    let data = JSON.stringify({
-      username: inp.value,
-      password: pass.value
-    });
-    localStorage.setItem("userdata", data);
+    localStorage.setItem("username", inp.value);
+    localStorage.setItem("password", pass.value);
   } else {
-    localStorage.removeItem("userdata");
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
   }
+
   alert(`Logged in as ${inp.value}`);
 });
 
 loginBtn.addEventListener("click", () => {
-  let stored = localStorage.getItem("userdata");
-  if (stored) {
-    let username = JSON.parse(stored).username;
+  let username = localStorage.getItem("username");
+  if (username) {
     alert(`Logged in as ${username}`);
   }
 });
