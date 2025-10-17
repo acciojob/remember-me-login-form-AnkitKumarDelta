da97ee33af622d71b5b8d6447bf1f551f315e6f2
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-10-17 09:31:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,24 +5,31 @@ let check = document.querySelector('#checkbox');
 let submitBtn = document.querySelector('#submit');
 let loginBtn = document.querySelector('#existing');
 
-loginBtn.disabled = !localStorage.getItem("userdata");
+loginBtn.style.display = "none";
+
+if (localStorage.getItem("userdata")) {
+  loginBtn.style.display = "inline";
+}
 
 f.addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	if (check.checked) {
-		let data = JSON.stringify({
-			username: inp.value,
-			password: pass.value
-		});
-		localStorage.setItem("userdata", data);
-	}
-else if(!check.checked){
-	localStorage.removeItem("userdata");
-}
-	alert(`Logged in as ${inp.value}`);
+  if (check.checked) {
+    let data = JSON.stringify({
+      username: inp.value,
+      password: pass.value
+    });
+    localStorage.setItem("userdata", data);
+  } else {
+    localStorage.removeItem("userdata");
+  }
+  alert(`Logged in as ${inp.value}`);
 });
-loginBtn.addEventListener("click",(e)=>{
-	let username = JSON.parse(localStorage.getItem("userdata")).username;
-	alert(`Logged in as ${username}`);
-})
+
+loginBtn.addEventListener("click", () => {
+  let stored = localStorage.getItem("userdata");
+  if (stored) {
+    let username = JSON.parse(stored).username;
+    alert(`Logged in as ${username}`);
+  }
+});
